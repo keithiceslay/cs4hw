ebd0b92c80464c8b26258a9def97590614c5b7ec
Keith <4restrei@gmail.com>
2022-10-29 18:02:26 +0300

    sadfg

---
+++ pr1/Program.cs
@@ -0,0 +1,10 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+// 3, 5 -> 243 (3⁵)
+
+// 2, 4 -> 16
+
+Console.WriteLine("Введите изначальное число и степень которого вы хотите получить");
+int number = int.Parse(Console.ReadLine());
+int power = int.Parse(Console.ReadLine());
+Console.WriteLine(Math.Pow(number, power));
---
+++ pr2/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+// 452 -> 11
+
+// 82 -> 10
+
+// 9012 -> 12
+
+Console.WriteLine("Введите число");
+string numberStr = Console.ReadLine();
+int number = int.Parse(numberStr);
+int sum = 0;
+int num = number;
+for (int i = 0; i<numberStr.Length; i++)
+{
+    sum = sum + num%10;
+    num = num/10;
+}
+Console.WriteLine(sum);
